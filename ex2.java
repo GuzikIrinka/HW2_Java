ae302f3221a0db472ce9af4d8417f2d3ab028c9f
Irina <andrey_k.90@mail.ru>
2022-11-21 22:38:01 +0300

    Задание 2

---
+++ ex2.java
@@ -0,0 +1,11 @@
+/*Напишитепрограмму для объединения данной строки с самим собой заданное количество раз */
+
+public class ex2 {
+    public static void main(String[] args) {
+        String s = "Welcome to GB";
+        String s1 = "Hello 2023";
+
+        System.out.println("Welcome to GB\n".repeat(3));
+        System.out.println("Hello 2023\n".repeat(5));
+    }
+}
